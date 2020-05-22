74805f94de0f3f7940d5631bf3dde84c5a8d7432
Symbol <Symbolistic@live.com>
2020-05-22 19:20:15 -0400

    Updates

---
+++ precache-manifest.df64afaf0d17a487fb939033917b5a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8e0fe3ff7372d729e70c47ae0fd9dc",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "67cdd0cbce705be3a16b",
+    "url": "/calculator-app/static/css/main.63498b17.chunk.css"
+  },
+  {
+    "revision": "e53ec218b396b398b550",
+    "url": "/calculator-app/static/js/2.bad39795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.bad39795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cdd0cbce705be3a16b",
+    "url": "/calculator-app/static/js/main.6dd2d035.chunk.js"
+  },
+  {
+    "revision": "e52fba28ccb6ce4c534d",
+    "url": "/calculator-app/static/js/runtime-main.0d33511c.js"
+  }
+]);
